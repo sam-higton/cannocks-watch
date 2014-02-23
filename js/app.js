abdd97be76195c2fea4c81eb65c2b7532e98e38c
Sam Higton <contact@samhigton.com>
2014-02-23 16:47:18 +0000

    restructuring to work with Angular JS (based on angular seed skeleton)

---
+++ js/app.js
@@ -0,0 +1,19 @@
+'use strict';
+
+
+// Declare app level module which depends on filters, and services
+angular.module('myApp', [
+  'ngRoute',
+  'myApp.filters',
+  'myApp.services',
+  'myApp.directives',
+  'myApp.controllers'
+]).
+config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/guards-office', {templateUrl: 'partials/guards-office.html', controller: 'GuardsOffice'});
+  $routeProvider.when('/barracks', {templateUrl: 'partials/barracks.html', controller: 'Barracks'});
+  $routeProvider.when('/armory', {templateUrl: 'partials/armory.html', controller: 'Armory'});
+  $routeProvider.when('/tavern', {templateUrl: 'partials/tavern.html', controller: 'Tavern'});
+  $routeProvider.when('/town-hall', {templateUrl: 'partials/town-hall.html', controller: 'TownHall'});
+  $routeProvider.otherwise({redirectTo: '/guards-office'});
+}]);
---
+++ js/controllers.js
@@ -0,0 +1,20 @@
+'use strict';
+
+/* Controllers */
+
+angular.module('myApp.controllers', []).
+    controller('GuardsOffice', [function() {
+
+    }])
+    .controller('Barracks', [function() {
+
+    }])
+    .controller('Armory', [function() {
+
+    }])
+    .controller('Tavern', [function() {
+
+    }])
+    .controller('TownHall', [function() {
+
+    }]);
